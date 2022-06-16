2743f5d266cd7abbb773593c6da37b537be11c14
JacobJanashvili <ghostiakobilord2@gmail.com>
2022-06-16 22:59:59 +0400

    edited my logic

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -4,10 +4,10 @@ const computerPlay = () => {
   let choice = Math.floor(Math.random() * items.length);
   return items[choice];
 };
-function Game(userSelectionItem,computerSelection) {
-  computerSelection=computerPlay()
+function Game(userSelectionItem, computerSelection) {
+  computerSelection = computerPlay();
   let userSelectionPlay = prompt("Do you want play?");
-  if (userSelectionPlay == "no" || userSelectionPlay == "" || userSelectionPlay !== "yes") {
+  if (userSelectionPlay !== "yes") {
     throw "ok exiting the game";
   } else {
     userSelectionItem = prompt("Choose item");
