5dbfcd8925fb18792494c20b560f1b1aa90605cb
JacobJanashvili <ghostiakobilord2@gmail.com>
2022-06-17 15:39:56 +0400

    Finished writing this program but need to ffix some things

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -6,6 +6,7 @@ const computerPlay = () => {
 };
 function Game(userSelectionItem, computerSelection) {
   computerSelection = computerPlay();
+  console.log(computerSelection)
   let userSelectionPlay = prompt("Do you want to play?");
   if (userSelectionPlay !== "yes") {
     throw "ok exiting the game";
@@ -14,6 +15,25 @@ function Game(userSelectionItem, computerSelection) {
     if (!items.includes(userSelectionItem)) {
       userSelectionItem = prompt("Please choose correct item");
     }
+    if(computerSelection=="Rock"&&userSelectionItem=="Paper"){
+      console.log("You win!")
+    }
+    else if(computerSelection=="Paper"&&userSelectionItem=="Rock"){
+      console.log("You Lose! Paper beats Rock")
+    }
+    else if(computerSelection=="Paper"&&userSelectionItem=="Scissors"){
+      console.log("You win!")
+    }
+    else if(computerSelection=="Scissors"&&userSelectionItem=="Paper"){
+      console.log("You Lose! Scissors beats Paper")
+    }
+    else if(computerSelection=="Rock"&&userSelectionItem=="Scissors"){
+      console.log("You Lose! Rock beats Scissors")
+    }
+    else if(computerSelection=="Scissors"&&userSelectionItem=="Rock"){
+      console.log("You Win!")
+    }
+    console.log("Draw")
   }
 }
 
