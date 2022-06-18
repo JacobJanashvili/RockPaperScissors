bce0149883e08740894208f465c39a520f3dce64
JacobJanashvili <ghostiakobilord2@gmail.com>
2022-06-19 02:19:13 +0400

    finally made scoring sytem functional but need to fix some more

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -1,39 +1,39 @@
 const items = ["rock", "paper", "scissors"];
-let winner = "";
+let winner;
 const computerPlay = () => {
-  let choice = Math.floor(Math.random() * items.length);
+  let choice = (Math.random() * items.length) | 0;
   return items[choice];
 };
 function playRound(userSelectionItem, computerSelection) {
-  computerSelection = computerPlay().toLowerCase();
+  computerSelection = computerPlay();
+  console.log(computerSelection);
+
   let userSelectionPlay = prompt("Do you want to play?");
   if (userSelectionPlay.toUpperCase() !== "YES") {
     throw "ok exiting the game";
-  } else {
-    userSelectionItem = prompt("Choose item").toLowerCase();
-    if (!items.includes(userSelectionItem)) {
-      userSelectionItem = prompt("Please choose correct item").toLowerCase();
-    }
-    if (computerSelection == "rock" && userSelectionItem == "paper") {
-      winner = "You win!";
-    } else if (computerSelection == "paper" && userSelectionItem == "rock") {
-      winner = "You Lose! Paper beats Rock";
-    } else if (
-      computerSelection == "paper" &&
-      userSelectionItem == "scissors"
-    ) {
-      winner = "You win!";
-    } else if (
-      computerSelection == "scissors" &&
-      userSelectionItem == "paper"
-    ) {
-      winner = "You Lose! Scissors beats Paper";
-    } else if (computerSelection == "rock" && userSelectionItem == "scissors") {
-      winner = "You Lose! Rock beats Scissors";
-    } else if (computerSelection == "scissors" && userSelectionItem == "rock") {
-      winner = "You Win!";
-    }
-    winner = "Draw";
+  }
+  userSelectionItem = prompt("Choose item");
+  console.log(userSelectionItem);
+  if (!items.includes(userSelectionItem)) {
+    userSelectionItem = prompt("Please choose correct item");
+  }
+  if (computerSelection == "rock" && userSelectionItem == "paper") {
+    winner = "You Win!";
+  }
+  if (computerSelection == "paper" && userSelectionItem == "rock") {
+    winner = "You Lose! Paper beats Rock";
+  }
+  if (computerSelection == "paper" && userSelectionItem == "scissors") {
+    winner = "You Win!";
+  }
+  if (computerSelection == "scissors" && userSelectionItem == "paper") {
+    winner = "You Lose! Scissors beats Paper";
+  }
+  if (computerSelection == "rock" && userSelectionItem == "scissors") {
+    winner = "You Lose! Rock beats Scissors";
+  }
+  if (computerSelection == "scissors" && userSelectionItem == "rock") {
+    winner = "You Win!";
   }
   return winner;
 }
@@ -41,19 +41,25 @@ function playRound(userSelectionItem, computerSelection) {
 function Game(userScore, computerScore) {
   userScore = 0;
   computerScore = 0;
-  console.log(computerPlay());
+
   for (let i = 0; i < 5; i++) {
     playRound();
-    if (winner.includes("win") === true) {
+    if (!winner.includes("Win") && !winner.includes("Lose")) {
+      userScore++;
+      computerScore++;
+    }
+    if (winner.includes("Win") == true) {
       userScore++;
     }
-
-    if (winner.includes("lose") === true) {
+    if (winner.includes("Lose") == true) {
       computerScore++;
     }
     console.log(`${computerScore} : ${userScore}`);
     if (i == 4 && computerScore > userScore) {
       console.log("You officially lose");
+    }
+    if (i == 4 && userScore > computerScore) {
+      console.log("Congrats You Win!");
     }
   }
 }
